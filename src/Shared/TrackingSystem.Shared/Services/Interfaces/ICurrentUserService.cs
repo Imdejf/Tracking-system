2e2fb4c4b39a9cdfd2002343939512a7c412b61c
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-06-29 19:15:03 +0200

    Added interfaces

---
+++ src/Shared/TrackingSystem.Shared/Services/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,11 @@
+﻿using TrackingSystem.Shared.Models;
+
+namespace TrackingSystem.Shared.Services.Interfaces
+{
+    public interface ICurrentUserService
+    {
+        JwtClaims CurrentUser { get; }
+        void SetCurrentUser(JwtClaims claims);
+        void SetCurrentUser(string jwt);
+    }
+}
---
+++ src/Shared/TrackingSystem.Shared/Services/Interfaces/ILoggingTool.cs
@@ -0,0 +1,10 @@
+﻿namespace TrackingSystem.Shared.Services.Interfaces
+{
+    public interface ILoggingTool
+    {
+        void LogInformation(string message);
+        void LogWarning(string message);
+        void LogError(string message);
+        void LogCritical(string message);
+    }
+}
