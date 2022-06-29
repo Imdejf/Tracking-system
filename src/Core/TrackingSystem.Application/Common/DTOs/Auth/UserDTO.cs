6c47b076572599ddda17f7c4f2dbffe0a86ad688
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-06-29 19:30:15 +0200

    Added user dto

---
+++ src/Core/TrackingSystem.Application/Common/DTOs/Auth/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace TrackingSystem.Application.Common.DTOs
+{
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? UserName { get; set; }
+        public string? LastName { get; set; }
+        public string? FullName => FirstName + " " + LastName;
+        public string? Position { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public IDictionary<string, IEnumerable<int>>? Permissions { get; set; }
+    }
+}
---
+++ src/Core/TrackingSystem.Application/Common/DTOs/Enum/EnumDTO.cs
@@ -0,0 +1,35 @@
+﻿namespace TrackingSystem.Application.Common.DTOs.Enum
+{
+    public class EnumDTO
+    {
+        public string? FieldName { get; set; }
+        public int FlagName { get; set; }
+
+        private EnumDTO(string? fieldName, int flagValue)
+        {
+            FieldName = fieldName;
+            FlagName = flagValue;
+        }
+
+        public static EnumDTO CreateForValue<TEnum>(TEnum @enum) where TEnum : System.Enum, IConvertible
+        {
+            if (!typeof(TEnum).IsEnum)
+            {
+                throw new ArgumentException("Passed parameter must be enumrated type");
+            }
+            return new EnumDTO(System.Enum.GetName(typeof(TEnum), @enum), @enum.ToInt32(null));
+        }
+
+        public static ICollection<EnumDTO> CreateForType<TEnum>() where TEnum : System.Enum, IConvertible
+        {
+            if (!typeof(TEnum).IsEnum)
+            {
+                throw new ArgumentException("Passed parameter must be enumrated type");
+            }
+            return System.Enum.GetValues(typeof(TEnum))
+                              .Cast<TEnum>()
+                              .Select(c => new EnumDTO(System.Enum.GetName(typeof(TEnum), c), c.ToInt32(null)))
+                              .ToList();
+        }
+    }
+}
