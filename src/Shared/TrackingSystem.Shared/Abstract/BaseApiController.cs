6dd3f63eaa93d6a241a7ecd0360ffde91b74e37e
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-06-29 18:56:41 +0200

    Added abstract class

---
+++ src/Shared/TrackingSystem.Shared/Abstract/BaseApiController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TrackingSystem.Shared.Abstract
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class BaseApiController : ControllerBase
+    {
+        private ISender _mediator = null!;
+
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
+}
---
+++ src/Shared/TrackingSystem.Shared/Abstract/ValueObject.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TrackingSystem.Shared.Abstract
+{
+    public abstract class ValueObject
+    {
+        protected abstract IEnumerable<object> GetEqualityComponents();
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (obj.GetType() != this.GetType())
+            {
+                throw new ArgumentException($"Invalid comparison of Value Objects of different types: {GetType()} and {obj.GetType()}");
+            }
+            var valueObj = (ValueObject)obj;
+
+            return this.GetEqualityComponents().SequenceEqual(valueObj.GetEqualityComponents());
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents().Aggregate(1, (a, b) => HashCode.Combine(a, b));
+        }
+
+        public static bool operator ==(ValueObject a, ValueObject b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+            {
+                return true;
+            }
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+            {
+                return false;
+            }
+            return a.Equals(b);
+        }
+        public static bool operator !=(ValueObject a, ValueObject b)
+        {
+            return !(a == b);
+        }
+    }
+}
