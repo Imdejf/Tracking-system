9fe0541f46dca957118255ad5c229955ba3ed499
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-06-29 19:21:21 +0200

    Added custom exceptions

---
+++ src/Core/TrackingSystem.Application/Common/Exceptions/IdentityException.cs
@@ -0,0 +1,18 @@
+﻿using JustCommmerce.Application.Models;
+using JustCommmerce.Shared.Abstract;
+
+namespace JustCommmerce.Application.Common.Exceptions
+{
+    public sealed class IdentityException : BaseAppException
+    {
+        public IdentityException(string message) : base(message)
+        {
+        }
+        public IdentityException(Dictionary<string, string[]> errors) : base(errors)
+        {
+        }
+        public IdentityException(IdentityActionResult identityActionResult) : base(new Dictionary<string, string[]>() { { "Message", identityActionResult.Errors } }) { }
+
+        public override int StatusCodeToRise => 400;
+    }
+}
