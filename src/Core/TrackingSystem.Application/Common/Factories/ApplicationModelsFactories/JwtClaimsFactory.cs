dd3ec783697d68031274f81754cc9af00bb07364
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-06-29 19:30:47 +0200

    Created wrapper for mediator

---
+++ src/Core/TrackingSystem.Application/Common/Factories/ApplicationModelsFactories/JwtClaimsFactory.cs
@@ -0,0 +1,20 @@
+﻿using TrackingSystem.Application.Models;
+
+namespace TrackingSystem.Application.Common.Factories.ApplicationModelsFactories
+{
+    public static class JwtClaimsFactory
+    {
+        public static JustCommerceJwtClaims CreateFromIntranetUserDTO(UserDTO dto)
+        {
+            return new JustCommerceJwtClaims
+            {
+                Email = dto.Email,
+                FirstName = dto.FirstName,
+                LastName = dto.LastName,
+                UserName = dto.UserName,
+                PermissionsList = dto.Permissions,
+                Id = dto.Id
+            };
+        }
+    }
+}
---
+++ src/Core/TrackingSystem.Domain/Common/BaseSeed.cs
@@ -0,0 +1,14 @@
+﻿namespace TrackingSystem.Domain.Common
+{
+    public class BaseSeed<T>
+    {
+        protected IEnumerable<T> Items;
+
+        public IEnumerable<T> GetItems() => Items;
+
+        public BaseSeed(IEnumerable<T> items)
+        {
+            Items = items;
+        }
+    }
+}
