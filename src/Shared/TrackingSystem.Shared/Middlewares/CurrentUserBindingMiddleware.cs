072ed28ce97e542918df6d60ef3d7d16d99e4a9b
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-06-29 19:15:23 +0200

    Added middlewares

---
+++ src/Shared/TrackingSystem.Shared/Middlewares/CurrentUserBindingMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using TrackingSystem.Shared.Services.Interfaces;
+using TrackingSystem.Shared.Services.Interfaces.JwtService;
+using Microsoft.AspNetCore.Http;
+
+namespace TrackingSystem.Shared.Middlewares
+{
+    public sealed class CurrentUserBindingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public CurrentUserBindingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, ICurrentUserService currentUserService, IJwtValidator jwtValidator)
+        {
+            var tokenSplit = context?.Request?.Headers["Authorization"].ToString().Split(" ");
+            if (tokenSplit.Length > 1)
+            {
+                var token = tokenSplit[1];
+                if (jwtValidator.IsValid(token))
+                {
+                    currentUserService.SetCurrentUser(token);
+                }
+            }
+            await _next(context);
+        }
+    }
+}
---
+++ src/Shared/TrackingSystem.Shared/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using TrackingSystem.Shared.Abstract;
+using TrackingSystem.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace TrackingSystem.Shared.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (BaseAppException baseAppException)
+            {
+                await throwError(context, baseAppException.StatusCodeToRise, baseAppException.Errors);
+            }
+            catch (Exception exception)
+            {
+                await throwError(context, 500, new Dictionary<string, string[]> { { "Message", new string[] { exception.Message } } });
+            }
+        }
+
+        private Task throwError(HttpContext context, int statusCode, Dictionary<string, string[]> errors)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+            response.StatusCode = statusCode;
+            return response.WriteAsync(JsonSerializer.Serialize(ApiResponse.Failure(statusCode, errors)));
+        }
+    }
+
+}
