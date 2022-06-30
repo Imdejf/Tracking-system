4806ba6c7c94978033d947e90b80da11e38d074d
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-06-30 21:44:51 +0200

    Add DI for infrastructure

---
+++ src/Infrastructure/TrackingSystem.Infrastructure/DependencyInjection/AddAppServices.cs
@@ -0,0 +1,51 @@
+﻿using DataSharp.EmailSender.DependencyInjection;
+using TrackingSystem.Application.Common.Interfaces;
+using TrackingSystem.Application.Common.Interfaces.CommonServices;
+using TrackingSystem.Application.Common.Interfaces.DataAccess.Service;
+using TrackingSystem.Application.Common.Interfaces.Notification;
+using TrackingSystem.Infrastructure.Configurations;
+using TrackingSystem.Infrastructure.Implementations;
+using TrackingSystem.Infrastructure.Implementations.Common;
+using TrackingSystem.Infrastructure.Implementations.Notifications;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TrackingSystem.Infrastructure.DependencyInjection
+{
+    public static partial class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAppServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var emailSenderConfig = new DataSharp.EmailSender.Models.EmailSenderConfig();
+            configuration.GetSection("Smtp").Bind(emailSenderConfig);
+
+            services.AddEmailSender(emailSenderConfig);
+            services.AddEmailTemplateProvider(c =>
+            {
+                c.AddTemplate("EmailConfirmation", a =>
+                    a.WithHtmlBodyFromFile("EmailTemplates/EmailConfirmation.html")
+                     .AddReplacementKey("[EMAILADDRESS]")
+                     .AddReplacementKey("[URL]")
+                );
+                c.AddTemplate("PasswordReset", a =>
+                    a.WithHtmlBodyFromFile("EmailTemplates/PasswordReset.html")
+                     .AddReplacementKey("[EMAILADDRESS]")
+                     .AddReplacementKey("[URL]")
+                );
+            });
+
+            services.Configure<MailLinksConfig>(configuration.GetSection("MailLinks"));
+            services.AddTransient<IMailSender, MailSender>();
+            services.AddTransient<IJwtGenerator, JwtGenerator>();
+            services.AddTransient<ITokenGenerator, TokenGenerator>();
+            services.AddTransient<INotificationHubClientWrapper, NotificationHubClientWrapper>();
+
+            services.AddSingleton<IUserIdsManager, UserIdsManager>();
+            services.AddSingleton<INotificationHubClient, NotificationHubClient>();
+
+            services.AddEmailTemplateProvider(c => { });
+
+            return services;
+        }
+    }
+}
---
+++ src/Infrastructure/TrackingSystem.Infrastructure/DependencyInjection/FtpManagerServices.cs
@@ -0,0 +1,49 @@
+﻿using DataSharp.FtpFileManagement.DependencyInjection;
+using TrackingSystem.Application.Common.Interfaces;
+using TrackingSystem.Infrastructure.Configurations;
+using TrackingSystem.Infrastructure.Implementations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using DataSharp.FtpFileManagement.Implementations;
+using DataSharp.FtpFileManagement.Interfaces;
+
+namespace TrackingSystem.Infrastructure.DependencyInjection
+{
+    public static partial class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddFtpFileManager(this IServiceCollection services, IConfigurationSection configurationSection)
+        {
+            FtpFileConfig config = new FtpFileConfig();
+            configurationSection.Bind(config);
+
+            services.AddFtpFileManager(c => {
+                c.AddConnection(config.Key, a =>
+                     a.WithHost(config.Host)
+                      .WithCredentails(config.UserId, config.Password)
+                      .UsingRootCatalog(config.UsingRootCatalog)
+                      .EnableSSL(config.EnableSSL)
+                      .UsePassive(config.UsePassive)
+                      .KeepAlive(config.KeepAlive)
+                      .WaitForReadWriteOperationExecutionFor(config.WaitForRequestExecutionFor)
+                      .MaximumNumberOfFilesThatCanBeRemovedAtOnce(config.MaximumNumberOfFilesThatCanBeRemovedAtOnce)
+                      .WaitForRequestExecutionFor(config.WaitForRequestExecutionFor)
+                      .OnDomainName(config.OnDomainName)
+
+                );
+            });
+            services.AddTransient<IFtpFileManager, FtpFileManager>();
+            return services;
+        }
+        public static IServiceCollection CreateFtpFolder(this IServiceCollection services)
+        {
+            DataSharp.FtpFileManagement.Interfaces.IFtpFileManager ftpFileManager = services.BuildServiceProvider().GetRequiredService<DataSharp.FtpFileManagement.Interfaces.IFtpFileManager>();
+            var currentConnection = ftpFileManager.GetCurrentConnection();
+            if (!ftpFileManager.DirectoryExists(currentConnection.Value.RootFolder))
+            {
+                ftpFileManager.CreateDirectory(currentConnection.Value.RootFolder);
+            }
+
+            return services;
+        }
+    }
+}
