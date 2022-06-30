1e2be790e2144e02fa432a2e2537c6a15f443ff3
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-06-30 21:37:57 +0200

    Added entities factories

---
+++ src/Core/TrackingSystem.Application/Common/Factories/EntitiesFactories/User/UserEntityFacotry.cs
@@ -0,0 +1,23 @@
+﻿using TrackingSystem.Application.Features.CommonFeatures.AuthFeatures.Command;
+using TrackingSystem.Domain.Entities.Identity;
+
+namespace TrackingSystem.Application.Common.Factories.EntitiesFactories.User
+{
+    public static class UserEntityFacotry
+    {
+        public static UserEntity CreateFromRegisterCommand(Register.Command command)
+        {
+            return new UserEntity
+            {
+                Email = command.Email,
+                FirstName = command.FirstName,
+                LastName = command.LastName,
+                PhoneNumber = command.PhoneNumber,
+                EmailConfirmed = false,
+                RegisterSource = command.RegisterSource,
+                UserName = command.Login,
+                CreatedDate = DateTime.Now,
+            };
+        }
+    }
+}
---
+++ src/Core/TrackingSystem.Application/Common/Factories/EntitiesFactories/User/UserPermissionEntityFactory.cs
@@ -0,0 +1,27 @@
+﻿using TrackingSystem.Application.Common.DTOs;
+using TrackingSystem.Domain.Entities.Identity;
+
+namespace TrackingSystem.Application.Common.Factories.EntitiesFactories.User
+{
+    public static class UserPermissionEntityFactory
+    {
+        public static UserPermissionEntity CreateFromDtoAndUserId(PermissionDTO dto, Guid userId)
+        {
+            return new UserPermissionEntity
+            {
+                PermissionDomainName = dto.PermissionDomainName,
+                PermissionFlagValue = dto.PermissionFlagValue,
+                UserId = userId
+            };
+        }
+        public static UserPermissionEntity CreateFromData(string domainName, int flagValue, Guid userId)
+        {
+            return new UserPermissionEntity
+            {
+                PermissionDomainName = domainName,
+                PermissionFlagValue = flagValue,
+                UserId = userId
+            };
+        }
+    }
+}
