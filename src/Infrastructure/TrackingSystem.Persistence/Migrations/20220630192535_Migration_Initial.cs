d72183dd773c3bec1ce7e5921c345494327d668d
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2022-06-30 21:36:57 +0200

    Added initial migration

---
+++ src/Infrastructure/TrackingSystem.Persistence/Migrations/20220630192535_Migration_Initial.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JustCommerce.Persistence.Migrations
+{
+    public partial class Migration_Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "identity");
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                schema: "identity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    RegisterSource = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    UserName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: false),
+                    NormalizedEmail = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: false),
+                    EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    PasswordHash = table.Column<string>(type: "varchar(max)", nullable: false),
+                    SecurityStamp = table.Column<string>(type: "varchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "varchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true),
+                    PhoneNumberConfirmed = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserPermission",
+                schema: "identity",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PermissionDomainName = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false),
+                    PermissionFlagValue = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPermission", x => new { x.PermissionDomainName, x.PermissionFlagValue, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_UserPermission_User_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "identity",
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPermission_UserId",
+                schema: "identity",
+                table: "UserPermission",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserPermission",
+                schema: "identity");
+
+            migrationBuilder.DropTable(
+                name: "User",
+                schema: "identity");
+        }
+    }
+}
